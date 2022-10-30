9dcead6579247e404bcbd3320e249aeecdd7d50d
Igor Pominov <ipominov@gmail.com>
2022-10-30 16:07:29 +0500

    Update terr.tf

--- terr.tf
+++ terr.tf
@@ -16,7 +16,7 @@ provider "yandex" {
 # VM BASTION
 
 resource "yandex_compute_instance" "bastion" {
-  name = "bastion"
+  name = "vm-bastion"
   zone = "ru-central1-a"
   allow_stopping_for_update = true
   resources {
@@ -34,8 +34,8 @@ resource "yandex_compute_instance" "bastion" {
     }
   }
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-public.id
-    security_group_ids = [yandex_vpc_security_group.bastion.id]
+    subnet_id = yandex_vpc_subnet.subnet-bastion.id
+    security_group_ids = [yandex_vpc_security_group.bastionsg.id]
     nat       = true
   }
   metadata = {
@@ -46,7 +46,7 @@ resource "yandex_compute_instance" "bastion" {
 # VM NGINX 1
 
 resource "yandex_compute_instance" "nginx-one" {
-  name = "nginx-1"
+  name = "vm-nginx-1"
   zone = "ru-central1-a"
   allow_stopping_for_update = true
   resources {
@@ -65,8 +65,8 @@ resource "yandex_compute_instance" "nginx-one" {
   }
   network_interface {
     subnet_id          = yandex_vpc_subnet.subnet-nginx-one.id
-    security_group_ids = [yandex_vpc_security_group.http-in.id]
-    nat                = false
+    security_group_ids = [yandex_vpc_security_group.nginxsg.id]
+    nat                = true
   }
   metadata = {
     user-data = "${file("./meta.txt")}"
@@ -76,7 +76,7 @@ resource "yandex_compute_instance" "nginx-one" {
 # VM NGINX 2
 
 resource "yandex_compute_instance" "nginx-two" {
-  name = "nginx-2"
+  name = "vm-nginx-2"
   zone = "ru-central1-b"
   allow_stopping_for_update = true
   resources {
@@ -95,8 +95,128 @@ resource "yandex_compute_instance" "nginx-two" {
   }
   network_interface {
     subnet_id          = yandex_vpc_subnet.subnet-nginx-two.id
-    security_group_ids = [yandex_vpc_security_group.http-in.id]
-    nat                = false
+    security_group_ids = [yandex_vpc_security_group.nginxsg.id]
+    nat                = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+# VM ELASTICSEARCH/LOGSTASH
+
+resource "yandex_compute_instance" "elasticsearch" {
+  name = "vm-elasticsearch"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 2
+    memory = 6
+    core_fraction = 20
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8t82r4n6u4rm577l9b"
+      size = 20
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet-elastcisearch.id
+    security_group_ids = [yandex_vpc_security_group.elasticsg.id]
+    nat                = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+# VM PROMETHEUS
+
+resource "yandex_compute_instance" "prometheus" {
+  name = "vm-prometheus"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 20
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8t82r4n6u4rm577l9b"
+      size = 20
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet-prometheus.id
+    security_group_ids = [yandex_vpc_security_group.prometheussg.id]
+    nat                = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+# VM KIBANA
+
+resource "yandex_compute_instance" "kibana" {
+  name = "vm-kibana"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 20
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8t82r4n6u4rm577l9b"
+      size = 10
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet-public.id
+    security_group_ids = [yandex_vpc_security_group.publicsg.id]
+    nat                = true
+  }
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+}
+
+# VM GRAFANA
+
+resource "yandex_compute_instance" "grafana" {
+  name = "vm-grafana"
+  zone = "ru-central1-a"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 20
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8t82r4n6u4rm577l9b"
+      size = 20
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.subnet-public.id
+    security_group_ids = [yandex_vpc_security_group.publicsg.id]
+    nat                = true
   }
   metadata = {
     user-data = "${file("./meta.txt")}"
@@ -109,73 +229,185 @@ resource "yandex_vpc_network" "network-netology" {
   name = "all-net"
 }
 
+resource "yandex_vpc_subnet" "subnet-bastion" {
+  name           = "sbn-bastion"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-netology.id
+  v4_cidr_blocks = ["192.168.9.0/24"]
+}
+
 resource "yandex_vpc_subnet" "subnet-public" {
-  name           = "subnet-bastion"
+  name           = "sbn-public"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-netology.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "yandex_vpc_subnet" "subnet-nginx-one" {
-  name           = "subnet-nginx-1"
+  name           = "sbn-nginx-1"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-netology.id
   v4_cidr_blocks = ["192.168.11.0/24"]
 }
 
 resource "yandex_vpc_subnet" "subnet-nginx-two" {
-  name           = "subnet-nginx-2"
+  name           = "sbn-nginx-2"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network-netology.id
   v4_cidr_blocks = ["192.168.12.0/24"]
 }
 
-# SECURITY GROUPS ------------------------------------------------------
-
-resource "yandex_vpc_security_group" "http-in" {
-  name        = "nginx-in"
-  description = "SG for nginx"
-  network_id  = "${yandex_vpc_network.network-netology.id}"
-  ingress {
-    protocol       = "TCP"
-    description    = "http in"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 80
-  }
-  ingress {
-    protocol       = "TCP"
-    description    = "ssh in"
-    v4_cidr_blocks = ["192.168.10.0/24"]
-    port           = 22
-  }
-  egress {
-    protocol       = "TCP"
-    description    = "all out tcp"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "yandex_vpc_subnet" "subnet-elastcisearch" {
+  name           = "sbn-elasticsearch"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-netology.id
+  v4_cidr_blocks = ["192.168.13.0/24"]
 }
 
-resource "yandex_vpc_security_group" "bastion" {
+resource "yandex_vpc_subnet" "subnet-prometheus" {
+  name           = "sbn-prometheus"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-netology.id
+  v4_cidr_blocks = ["192.168.14.0/24"]
+}
+
+# SECURITY GROUPS ------------------------------------------------------
+
+resource "yandex_vpc_security_group" "bastionsg" {
   name        = "bastion-sg"
   description = "SG for bastion"
   network_id  = "${yandex_vpc_network.network-netology.id}"
   ingress {
     protocol       = "TCP"
-    description    = "ssh in"
+    description    = "All in"
     v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol       = "TCP"
+    description    = "All out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "publicsg" {
+  name        = "public-sg"
+  description = "SG for bastion"
+  network_id  = "${yandex_vpc_network.network-netology.id}"
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion"
+    v4_cidr_blocks = ["192.168.9.0/24"]
     port           = 22
   }
-  egress {
+  ingress {
     protocol       = "TCP"
-    description    = "http to nginx1 sg"
-    v4_cidr_blocks = ["192.168.11.0/24", "192.168.12.0/24"]
-    port        = 80
+    description    = "in kibana"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 5601
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "in grafana"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 3000
   }
   egress {
     protocol       = "TCP"
-    description    = "ssh to nginx2 sg"
+    description    = "All out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "nginxsg" {
+  name        = "nginx-sg"
+  description = "SG for nginx"
+  network_id  = "${yandex_vpc_network.network-netology.id}"
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion"
+    v4_cidr_blocks = ["192.168.9.0/24"]
+    port           = 22
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "All HTTP in"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "All HTTPS in"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "node exporter in"
+    v4_cidr_blocks = ["192.168.14.0/24"]
+    port           = 9100
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "nginx log exporter in"
+    v4_cidr_blocks = ["192.168.14.0/24"]
+    port           = 4040
+  }
+  egress {
+    protocol       = "TCP"
+    description    = "All out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "elasticsg" {
+  name        = "elastic-sg"
+  description = "SG for elastic and logstash"
+  network_id  = "${yandex_vpc_network.network-netology.id}"
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion"
+    v4_cidr_blocks = ["192.168.9.0/24"]
+    port           = 22
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP to Elastic"
+    v4_cidr_blocks = ["192.168.10.0/24"]
+    port           = 9200
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "to logstash from beats"
     v4_cidr_blocks = ["192.168.11.0/24", "192.168.12.0/24"]
-    port        = 22
+    port           = 5044
+  }
+  egress {
+    protocol       = "TCP"
+    description    = "All out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "prometheussg" {
+  name        = "prometheus-sg"
+  description = "SG for prometheus"
+  network_id  = "${yandex_vpc_network.network-netology.id}"
+  ingress {
+    protocol       = "TCP"
+    description    = "SSH from bastion"
+    v4_cidr_blocks = ["192.168.9.0/24"]
+    port           = 22
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "HTTP in"
+    v4_cidr_blocks = ["192.168.10.0/24"]
+    port           = 9090
+  }
+  egress {
+    protocol       = "TCP"
+    description    = "All out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
